276eacf5916a9d1f1c6ee40a0938cb5526277f6f
eat-sleep-code <info@eat-sleep-code.com>
2021-03-30 16:10:00 -0700

    Forgot an equals

--- server.py
+++ server.py
@@ -96,7 +96,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 class Server():
 	def startStream(camera, running, statusDictionary, buttonDictionary):
-		camera.resolution(960, 540)
+		camera.resolution = (960, 540)
 		camera.framerate = 24
 		with camera:
 			output = StreamingOutput()
